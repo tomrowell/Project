2a2d81887087ab574819f86a0bbf308bbccc51cf
- <->
2016-03-08 15:25:04 +0000

    Updated Second Iteration
    Added functionality to test corner vertices found
    Fixed a slight offset in constructShape.m which placed the second shape
    too far to the right
    Decreased the sensitivity of the Corner function to find less vertices
    Increased the uncertainty range when comparing vertices in
    compareVertices.m

--- Project/Version 2/compareVertices.m
+++ Project/Version 2/compareVertices.m
@@ -1,6 +1,6 @@
 function [bestDisparity1, bestDisparity2] = compareVertices(shape1, shape2)
-vertices1 = corner(shape1, 'SensitivityFactor', 0.00001);
-vertices2 = corner(shape2, 'SensitivityFactor', 0.00001);
+vertices1 = corner(shape1, 'SensitivityFactor', 0.2);
+vertices2 = corner(shape2, 'SensitivityFactor', 0.2);
 
 bestCount = 0;
 bestDisparity1 = 0;
@@ -20,7 +20,8 @@ for i = 1:length(vertices1)
        
       for m = 1:length(vertices1)
          for n = 1:length(vertices2Temp)
-            if (((vertices1(m, 1) == vertices2Temp(n, 1)) || (vertices1(m, 1) == vertices2Temp(n, 1)+1) || (vertices1(m, 1) == vertices2Temp(n, 1)-1)) && (((vertices1(m,2) == vertices2Temp(n, 2)) || (vertices1(m,2) == vertices2Temp(n, 2)+1) || (vertices1(m,2) == vertices2Temp(n, 2)-1))))
+            if ((vertices2Temp(n, 1)-3 <= vertices1(m, 1) && vertices1(m, 1) <= vertices2Temp(n, 1)+3) && (vertices2Temp(n, 2)-3 <= vertices1(m, 2) && vertices1(m, 2) <= vertices2Temp(n, 2)+3))
+            %if (((vertices1(m, 1) == vertices2Temp(n, 1)) || (vertices1(m, 1) == vertices2Temp(n, 1)+1) || (vertices1(m, 1) == vertices2Temp(n, 1)-1)) && (((vertices1(m,2) == vertices2Temp(n, 2)) || (vertices1(m,2) == vertices2Temp(n, 2)+1) || (vertices1(m,2) == vertices2Temp(n, 2)-1))))
                if (m ~= i || n ~= j)
                   counter = counter + 1;
                   if (counter > bestCount)
--- Project/Version 2/constructShape.m
+++ Project/Version 2/constructShape.m
@@ -6,5 +6,5 @@ shape2 = imcrop(shape2,Sdata(1).BoundingBox);
 [y1,x1] = size(shape1);
 [y2,x2] = size(shape2);
 shapeTemp = zeros(y1,x1);
-shapeTemp(((y1-y2)/2)+disparity2:(((y1-y2)/2))+y2+disparity2-1, ((x1-x2)/2)+disparity1+1:(((x1-x2)/2))+x2+disparity1) = shape2;
+shapeTemp(((y1-y2)/2)+disparity2:(((y1-y2)/2))+y2+disparity2-1, ((x1-x2)/2)+disparity1:(((x1-x2)/2))+x2+disparity1-1) = shape2;
 completeShape = shape1 + shapeTemp;
